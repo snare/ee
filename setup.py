d798db534b45af55319553be317967349094c048
snare <snare@ho.ax>
2016-08-27 23:36:24 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = "ee",
-    version = "0.3",
-    author = "snare",
-    author_email = "snare@ho.ax",
-    description = ("A wrapper for dd"),
-    license = "Buy snare a beer",
-    keywords = "dd wrapper",
-    url = "https://github.com/snare/ee",
+    name="ee",
+    version="0.4",
+    author="snare",
+    author_email="snare@ho.ax",
+    description=("A wrapper for dd"),
+    license="Buy snare a beer",
+    keywords="dd wrapper",
+    url="https://github.com/snare/ee",
     packages=['ee'],
-    install_requires = ['blessed'],
-    entry_points = {'console_scripts': ['ee = ee:main']}
+    install_requires=['blessed'],
+    entry_points={'console_scripts': ['ee=ee:main']}
 )
